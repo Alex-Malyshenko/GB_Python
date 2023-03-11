e7d6dc1e4003e2687e2d309884c6db8e264151ea
Alex <alis_mal@mail.ru>
2023-03-11 19:06:48 +0300

    Task_2 end

--- Task_1.py
+++ Task_1.py
@@ -5,7 +5,7 @@
 
 numbers = int(input("Enter a three-digit number: "))
 s = 0
-for i in range(3):
+for _ in range(3):
     s += numbers % 10
     numbers = numbers // 10
-print(f"The sum of the digits of this number is equal to : {s}")
+print(f"The sum of the digits of this number is equal to: {s}")
