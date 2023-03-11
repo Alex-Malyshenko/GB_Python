1619e8647591e6ab4cebfda885d8d4aea4fc24f3
Alex <alis_mal@mail.ru>
2023-03-11 18:32:31 +0300

    Task end

--- Task_1.py
+++ Task_1.py
@@ -3,3 +3,9 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
+numbers = int(input("Enter a three-digit number: "))
+s = 0
+for i in range(3):
+    s += numbers % 10
+    numbers = numbers // 10
+print(f"The sum of the digits of this number is equal to : {s}")
